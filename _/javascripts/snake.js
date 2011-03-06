6ce499a573059a785ffe3d15c477155d1e067530
Gregor Martynus <gregor@martynus.net>
2011-03-06 11:41:23 +0100

    kicked canvas, using HTML DOM elements. More to come.

---
+++ _/javascripts/snake.js
@@ -0,0 +1,117 @@
+var Snake = function(options) {
+  options = options || {};
+  
+  // CONSTANTS
+  var STOP   = 0,
+      UP     = 1,
+      RIGHT  = 2, 
+      DOWN   = 3,
+      LEFT   = 4,
+      
+      DIRECTIONS = {
+        0: {x:  0, y:  0}, // STOP   
+        1: {x:  0, y: -1}, // UP   
+        2: {x:  1, y:  0}, // RIGTH
+        3: {x:  0, y:  1}, // DOWN 
+        4: {x: -1, y:  0}  // LEFT 
+      };
+  
+  // DEFAULTS
+  var _defaults = {
+    length    : 3,
+    direction : UP
+  };
+  
+  // PUBLIC
+  this.move = function()
+  {
+    _turn();
+
+    // move head in current direction
+    _parts[0].direction = _direction;
+
+    // apply move
+    for (var i=0; i < _parts.length; i++) {
+      _flagAsFree(_parts[i].x,_parts[i].y)
+      _parts[i].x += DIRECTIONS[_parts[i].direction].x;
+      _parts[i].y += DIRECTIONS[_parts[i].direction].y;
+      _onPartMove(i, _parts[i].x, _parts[i].y);
+      
+      if ( i == 0) console.log(_taken_pos, _parts[i].x, _parts[i].y)
+      
+      if ( i == 0 && this.isPosTaken(_parts[i].x, _parts[i].y) ) _game_over = true;
+      _flagAsTaken(_parts[i].x,_parts[i].y)
+    };
+    
+    if (_game_over) _onCollision();
+
+    // inherit direction from part before
+    for (var i = _parts.length - 1; i > 0; i--){
+      // if the current part is not moving
+      if (_parts[i].direction == STOP) {
+        // don't inherit direction unless it has been passed by the part before
+        if (_parts[i].x + DIRECTIONS[_parts[i-1].direction].x == _parts[i-1].x &&
+            _parts[i].y + DIRECTIONS[_parts[i-1].direction].y == _parts[i-1].y)
+            _parts[i].direction = _parts[i-1].direction;
+      } else {
+        _parts[i].direction = _parts[i-1].direction;
+      }
+    };
+  };
+  
+  this.turnLeft = function() {
+    _turn_queue.push(-1);
+  };
+
+  this.turnRight = function() {
+    _turn_queue.push(1);
+  };
+  
+  this.addNewPart = function() {
+    _parts.push({
+      x         : _head.x,
+      y         : _head.y,
+      direction : STOP
+    });
+    
+    _onNewPart(_head.x, _head.y);
+  };
+  
+  this.isPosTaken = function(x, y) {
+    return _taken_pos[ [x,y].join(',') ];
+  };
+
+  // PRIVATE
+  var _direction   = options.direction || _defaults.direction,
+      _init_length = options.length    || _defaults.length,
+      _head        = {x:0,y:0,direction: _direction},
+      _parts       = [_head],
+      _turn_queue  = [],
+      _taken_pos   = {},
+      _game_over   = false,
+      _do_nothing  = function() {},
+      
+      _onNewPart   = options.onNewPart   || nada,
+      _onPartMove  = options.onPartMove  || nada,
+      _onCollision = options.onCollision || nada;
+  
+  var _turn = function() 
+  {
+    if (_turn_queue.length) {
+      var to = _turn_queue.shift();
+      _direction = [LEFT, UP, RIGHT, DOWN, LEFT, UP][_direction + to]; 
+    }
+  };
+  var _flagAsTaken = function(x,y) {
+    _taken_pos[ [x,y].join(',') ] = 1;
+  }
+  var _flagAsFree = function(x,y) {
+    delete _taken_pos[ [x,y].join(',') ];
+  }
+  
+  // create body parts
+  for (var i=1; i < _init_length; i++) this.addNewPart();
+  
+  // trigger newPart event for head
+  _onNewPart(_head.x, _head.y);
+};
